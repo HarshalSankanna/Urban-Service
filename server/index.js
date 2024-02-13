e1592311e78c8fbcf9d052aae20cf85b910cb5db
Harshal <harshal0921@gmail.com>
2024-02-13 13:58:53 +0530

    Start server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
